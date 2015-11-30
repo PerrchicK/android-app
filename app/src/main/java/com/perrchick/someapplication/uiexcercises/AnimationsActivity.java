ec2e7e698b493a4646998c16b040f8d77da42ed4
Perry Shalom <perrchick@Perrys-MacBook-Air.local>
2015-11-30 02:57:47 +0200

    Added animations' examples

---
+++ app/src/main/java/com/perrchick/someapplication/uiexcercises/AnimationsActivity.java
@@ -0,0 +1,153 @@
+package com.perrchick.someapplication.uiexcercises;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.perrchick.someapplication.R;
+import com.perrchick.someapplication.utilities.PerrFuncs;
+
+public class AnimationsActivity extends AppCompatActivity {
+    ImageView spinnerView;
+    private RotateAnimation rotateAnimation;
+    private TextView txtScaleValue;
+    private SeekBar scaleSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animations);
+
+        this.scaleSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        this.scaleSeekBar.setProgress(6);
+        this.scaleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress += 1;
+                scaleImage(progress);
+
+                ObjectAnimator fade = ObjectAnimator.ofFloat(txtScaleValue, "alpha", txtScaleValue.getAlpha(), (float)progress / 10.0f);
+                long duration = 300;
+                fade.setDuration(duration);
+                fade.start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        this.spinnerView = (ImageView) findViewById(R.id.spinnerImage);
+        this.spinnerView.setImageResource(R.drawable.ic_spinner_image);
+        this.spinnerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleSpinnerAnimation();
+            }
+        });
+
+        this.txtScaleValue = (TextView) findViewById(R.id.txtScaleValue);
+        this.txtScaleValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerrFuncs.sayNo(v);
+            }
+        });
+    }
+
+    private void scaleImage(float scaleSize) {
+        if (scaleSize > 0) { // Check anyway to prevent exceptions
+            txtScaleValue.setText("" + scaleSize);
+
+            scaleSize *= 0.1;
+            ObjectAnimator scaleImageX = ObjectAnimator.ofFloat(spinnerView, "scaleX", spinnerView.getScaleX(), scaleSize);
+            scaleImageX.setDuration(200);
+            scaleImageX.start();
+
+            ObjectAnimator scaleImageY = ObjectAnimator.ofFloat(spinnerView, "scaleY", spinnerView.getScaleY(), scaleSize);
+            scaleImageY.setDuration(200);
+            scaleImageY.start();
+        }
+    }
+
+    private void toggleSpinnerAnimation(boolean start) {
+        if (start) {
+            rotateAnimation = getRotateAnimation(this.spinnerView);
+            rotateAnimation.setRepeatCount(-1);
+            rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    rotateAnimation = null;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+            this.spinnerView.startAnimation(rotateAnimation);
+        } else {
+            rotateAnimation.setRepeatCount(0);
+        }
+    }
+
+    private void toggleSpinnerAnimation() {
+        this.toggleSpinnerAnimation(rotateAnimation == null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        scaleImage(this.scaleSeekBar.getProgress());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_animations, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public RotateAnimation getRotateAnimation(View viewToRotate) {
+        RotateAnimation rotateAnimation = new RotateAnimation(0f, -360f,viewToRotate.getWidth() / 2.0f,viewToRotate.getHeight() / 2.0f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(500);
+        rotateAnimation.setFillAfter(true);
+
+        return rotateAnimation;
+    }
+}
---
+++ app/src/main/java/com/perrchick/someapplication/uiexcercises/ImageDownload.java
@@ -0,0 +1,131 @@
+package com.perrchick.someapplication.uiexcercises;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.perrchick.someapplication.R;
+import com.perrchick.someapplication.utilities.PerrFuncs;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ImageDownload extends AppCompatActivity {
+
+    OkHttpClient client = new OkHttpClient(); // An open source project, downloaded from gradle
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        // Get the intent that started this activity
+        Intent intent = getIntent();
+        if (intent != null) {
+            if (intent.getStringExtra("data") != null) {
+                TextView dataLabel = (TextView) findViewById(R.id.dataText);
+                dataLabel.setText(intent.getStringExtra("data"));
+            }
+
+            Uri data = intent.getData();
+            if (intent.getType() != null) {
+                // Figure out what to do based on the intent type
+                if (intent.getType().indexOf("image/") != -1) {
+                    // Handle intents with image data ...
+                } else if (intent.getType().equals("text/plain")) {
+                    // Handle intents with text ...
+                }
+            }
+        }
+
+        // Initialize OkHttpClient
+        client.setReadTimeout(20, TimeUnit.SECONDS);
+
+        /** CLASS EXERCISE 05 - UPDATE UI AFTER NETWORK REQUEST **/
+
+        // Initialize clickable ImageView
+        final ImageView imageView = (ImageView) findViewById(R.id.image_from_web);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We're going to make a web request (network operation), we don't know how long is it going to take
+                new Thread(new Runnable() {
+                    public void run() {
+                        // The "long operation" line
+                        final Bitmap bmpImage = loadImageFromNetwork("http://www.comicsandmemes.com/wp-content/uploads/not-sure-fry-meme-pink-floyd-song.jpg");
+
+                        if (bmpImage != null) {
+                            // We shouldn't update UI in the worker thread, only on the UI Thread (main thread)
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imageView.setImageBitmap(bmpImage);
+                                }
+                            });
+                        } else {
+                            PerrFuncs.toast("Error downloading the image");
+                        }
+                    }
+                }).start();
+            }
+        });
+
+    }
+
+    /**
+     * Makes a network request and fetches an image from the specified URL.
+     *
+     * @param url The URL that represents the image's location on the web.
+     * @return A Bitmap object if the request succeeded, otherwise it returns null.
+     */
+    private Bitmap loadImageFromNetwork(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+
+            // The image's data is here
+            return BitmapFactory.decodeStream(response.body().byteStream());
+        } catch (IOException e) {
+            e.printStackTrace(); // Will print the stack trace in the "locgcat"
+        }
+
+        // There was an error
+        return null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_another, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
