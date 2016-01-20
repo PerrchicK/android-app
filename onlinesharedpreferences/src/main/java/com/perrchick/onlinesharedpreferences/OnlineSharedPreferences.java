e6a264ef00e02a17f37b63b57e352e2375bba3c4
Perry <perrchick@gmail.com>
2016-01-20 12:45:08 +0200

    Steps to make OnlineSharedPreferences to be independent and downloadable gradle project, now using the MAVEN REPO :)

--- onlinesharedpreferences/src/main/java/com/perrchick/onlinesharedpreferences/OnlineSharedPreferences.java
+++ onlinesharedpreferences/src/main/java/com/perrchick/onlinesharedpreferences/OnlineSharedPreferences.java
@@ -1,7 +1,6 @@
 package com.perrchick.onlinesharedpreferences;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.parse.DeleteCallback;
